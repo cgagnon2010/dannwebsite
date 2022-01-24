6fa3db3c50a0d5d3ce58a0d606a64537a97d4ae9
Christian Gagnon <cgagnon2010@live.com>
2022-01-23 21:32:13 -0500

    initial commit of danns website project

---
+++ dann/src/main/java/com/danns/website/dann/DannsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.danns.website.dann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DannsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DannsWebsiteApplication.class, args);
+	}
+
+}
---
+++ dann/src/test/java/com/danns/website/dann/DannsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danns.website.dann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DannsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
